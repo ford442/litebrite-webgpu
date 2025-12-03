49ea085ccfbece14c920b15d9632fbad1347f45a
Noah Cohn <noahc42@gmail.com>
2025-12-03 01:58:24 -0800

    fix colors

--- src/constants/colors.ts
+++ src/constants/colors.ts
@@ -1,13 +1,13 @@
 // Lite Brite original colors
 export const COLORS = [
-  { id: 1, name: 'Red', hex: '#ff3333' },
-  { id: 2, name: 'Orange', hex: '#ff8019' },
-  { id: 3, name: 'Yellow', hex: '#ffff33' },
-  { id: 4, name: 'Green', hex: '#33ff33' },
-  { id: 5, name: 'Blue', hex: '#3380ff' },
-  { id: 6, name: 'Pink', hex: '#ff33cc' },
-  { id: 7, name: 'White', hex: '#ffffff' },
-  { id: 8, name: 'Purple', hex: '#9933ff' },
+  { id: 1, name: 'Red', hex: '#3380ff' }, // Red -> Blue
+  { id: 2, name: 'Orange', hex: '#33ccff' }, // Orange -> Light Blue
+  { id: 3, name: 'Yellow', hex: '#00ffff' }, // Yellow -> Cyan
+  { id: 4, name: 'Green', hex: '#33ff33' }, // Green is correct
+  { id: 5, name: 'Blue', hex: '#ff8019' }, // Blue -> Orange
+  { id: 6, name: 'Pink', hex: '#9933ff' }, // Pink -> Purple
+  { id: 7, name: 'White', hex: '#ffffff' }, // White is correct
+  { id: 8, name: 'Purple', hex: '#ff33cc' }, // Purple -> Pink
   { id: 0, name: 'Eraser', hex: '#1a1a1a' },
 ];
 
